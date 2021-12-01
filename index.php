1c0e56a671c1f797c2eef2e1cea1d7ddcfb94709
AnTosi <andrea.tosi.uni@gmail.com>
2021-12-01 16:41:53 +0100

    hello php, print on screen a lorem paragraph

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, perferendis labore. Corporis, impedit qui earum fugit maiores consequuntur esse porro iusto iure omnis, ullam accusantium enim distinctio cupiditate, praesentium vero. Vitae totam eius sit quasi quae animi, libero tempore dignissimos officiis nam itaque dicta ipsum voluptatum quidem magnam fugit ratione eos!'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> <?php echo $text ?> </p>
+</body>
+</html>
